992a8d7e0c456957823a09a57cd286f2db510039
yangjun <yangjun1120@gmail.com>
2013-03-17 18:53:58 +0800

    create new blog

---
+++ meta.js
@@ -0,0 +1,13 @@
+{
+    "site_name": "杨军的博客",
+    "copyright": "YangJun 2013",
+    "cates": [
+        {"name": "tech", "text":"技术"},
+        {"name": "life", "text":"生活"},
+        {"name": "other", "text":"其它"}
+    ],
+    "articles":[
+        {"title": "Oozie调用sqoop导数据出现NoClassDefFoundError问题", "file": "2013-03-17", "cate":"tech"}
+        
+        ]
+}
