9af3c87a247759cb026f47508abb6e677685880d
yangjun <yangjun1120@gmail.com>
2013-03-17 20:38:37 +0800

    modify category

--- meta.js
+++ meta.js
@@ -2,12 +2,13 @@
     "site_name": "杨军的博客",
     "copyright": "YangJun 2013",
     "cates": [
-        {"name": "tech", "text":"技术"},
-        {"name": "life", "text":"生活"},
-        {"name": "other", "text":"其它"}
+        {"name": "hadoop", "text":"Hadoop"},
+        {"name": "hadoop", "text":"Spring"},
+        {"name": "life", "text":"Life"},
+        {"name": "other", "text":"Other"}
     ],
     "articles":[
-        {"title": "大数据笔记(一)", "file": "2013-01-17-blog_bigdatanote1", "cate":"tech"},
-        {"title": "Oozie调用sqoop导数据出现NoClassDefFoundError问题", "file": "2013-03-17-blog_oozie-sqoop-notdeffound", "cate":"tech"}
+        {"title": "大数据笔记(一)", "file": "2013-01-17-blog_bigdatanote1", "cate":"hadoop"},
+        {"title": "Oozie调用sqoop导数据出现NoClassDefFoundError问题", "file": "2013-03-17-blog_oozie-sqoop-notdeffound", "cate":"hadoop"}
         ]
 }
