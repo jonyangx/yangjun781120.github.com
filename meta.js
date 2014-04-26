5f7af9b9eb64e0cb717982bcc0630b3b92913589
yangjun <yangjun1120@gmail.com>
2014-04-27 00:18:39 +0800

    新增2013年多博客
    新增了2013年多几篇博客

--- meta.js
+++ meta.js
@@ -6,8 +6,14 @@
         {"name": "life", "text":"生活"}
     ],
     "articles":[
-        {"title": " Spring3.2 Http 请求处理过程笔记", "file": "2012-12-21-blog_springhttpprocess", "cate":"tech"},
-        {"title": "大数据笔记(一)", "file": "2013-01-17-blog_bigdatanote1", "cate":"tech"},
-        {"title": "Oozie调用sqoop导数据出现NoClassDefFoundError问题", "file": "2013-03-17-blog_oozie-sqoop-notdeffound", "cate":"tech"}
+        {"title": " Spring3.2 Http 请求处理过程笔记", "file": "2012-12-21-blog_01", "cate":"tech"},
+        {"title": "大数据笔记(一)", "file": "2013-01-17-blog_01", "cate":"tech"},
+        {"title": "Oozie调用sqoop导数据出现NoClassDefFoundError问题", "file": "2013-03-17-blog_01", "cate":"tech"},
+        {"title": "hadoop集群hdfs磁盘划分", "file": "2013-03-27-blog_01", "cate":"tech"},
+        {"title": "linux下jdb远程调试tomcat源码", "file": "2013-05-13-blog_01", "cate":"tech"},
+        {"title": "hive rownumber内置函数使用例子", "file": "2013-07-16-blog_01", "cate":"tech"},
+        {"title": "Hadoop MapReduce Task Log 无法查看syslog问题", "file": "2013-11-15-blog_01", "cate":"tech"},
+        {"title": "常用文本编辑工具列模式记录", "file": "2013-11-15-blog_02", "cate":"tech"}
+
         ]
 }
